afa829c44a70f1398ad6c7befd95da92827c4393
Angore-78 <allan.angore@gmail.com>
2025-07-10 17:36:33 +0300

    Creatte basic_operations.py and performed operations on number1 and number2

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+#calculates the sum of number1 and number2
+summation = number1 + number2
+#Calculates the difference by subtracting number2 from number1
+difference = number1 - number2
+#Calculate the result of multiplyiing number1 and number2
+product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {summation}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
