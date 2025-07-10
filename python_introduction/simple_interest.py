8a8380ed11fd83f9c75971e1e29a52e2302ad4e2
Angore-78 <allan.angore@gmail.com>
2025-07-10 18:42:31 +0300

    Your age in 2050

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,4 +3,4 @@ rate = 0.05
 time = 3
 # Simple interest as the product of principal, rate and time
 interest = principal * rate * time
-print(f"The simple interest is {interest}")
+print(f"The simple interest is: {interest}")
