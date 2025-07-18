b7b05456b1e68edc024f6d2c3ffeb63eb97d4ebe
Angore-78 <allan.angore@gmail.com>
2025-07-18 13:36:04 +0300

    included missing 'int' type for input

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,7 @@
-pattern_size = input('Enter the of the pattern :')
+pattern_size = int(input('Enter the of the pattern :'))
 output_count = 0
-while output_count != int(pattern_size):
+while output_count != (pattern_size):
     output_count +=1
-    if output_count>int(pattern_size):
+    if output_count>(pattern_size):
         break
-    print(int(pattern_size) * '*')
+    print((pattern_size) * '*')
