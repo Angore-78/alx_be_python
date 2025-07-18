fcd1aebf0375bde3d1103c9cc5a70ee757f29d42
Angore-78 <allan.angore@gmail.com>
2025-07-18 09:34:41 +0300

    Add control_flow

---
+++ control_flow/weather_advice.py
@@ -0,0 +1,13 @@
+#Receives weather input for the day and gives clothing recommendations
+weather_input = input("What's the weather like today?(sunny/rainy/cold):")
+#Conditions for clothing recommendations based on the weather input
+if weather_input == 'sunny' :
+    print("Wear a t-shirt and sunglasses.")
+elif weather_input == 'rainy' : 
+    print("Don't forget your umbrella and a raincoat.")
+elif weather_input == 'cold':
+    print("Make sure to wear a warm coat and scarf.")
+
+else:
+    print("Sorry I don't have recommendations for this weather.")
+    
