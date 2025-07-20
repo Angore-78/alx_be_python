262fcc8c553bd4cf9ec707b07fb4a5f26ed2b8d7
Angore-78 <allan.angore@gmail.com>
2025-07-20 11:18:48 +0300

    correct daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,24 @@
-task = input("Enter your task: ")
-Priority = input('Priority(high/medium/low): ')
-time_bound = input('Is it time bound?(yes/no): ')
+Task = input('Enter your task: ')
+Priority = input('Priority (high/medium/low): ')
+Time_bound = input('Is it time-bound? (yes/no): ')
 match Priority:
     case 'high':
-        if time_bound == 'yes':
-            print( f'{task} is a high priority task that requires immediate attention today!')
+        if Time_bound == 'yes':
+            print( f"Reminder:'{Task}' is a high priority task that requires immediate attention today!")
         else:
-            print(f'{task} is a high priority task.Consider completing it when you have free time.')
+            print(f"Note: '{Task}' is a high priority task.Consider completing it when you have free time.")
     case 'medium' :
-        if time_bound == 'yes':
-             print( f'{task} is a medium priority task that requires immediate attention today!')
+        if Time_bound == 'yes':
+             print( f"Reminder:'{Task}' is a medium priority task that requires immediate attention today!")
         else:
-            print(f'{task} is a medium priority task.Consider completing it when you have free time.')         
+            print(f"Note: '{Task}' is a medium priority task.Consider completing it when you have free time.")         
     case 'low':
-        if time_bound == 'yes':
-            print ( f'{task} is a low priority task that requires immediate attention today!')
+        if Time_bound == 'yes':
+            print ( f"Reminder:'{Task}' is a low priority task that requires immediate attention today!")
 
         else:
-            print(f'{task} is a low priority task.Consider completing it when you have free time.')
+            print(f"Note: '{Task}' is a low priority task.Consider completing it when you have free time.")
     case _:
-        print(f'{task} is an invalid input')
+        print(f'{Task} is an invalid input')
+
+    
