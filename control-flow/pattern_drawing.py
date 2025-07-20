7702ba5e277d7c6e607fe4f431e90fdd93fdf80d
Angore-78 <allan.angore@gmail.com>
2025-07-20 11:11:22 +0300

    change pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,6 @@
-pattern_size = input('Enter the size of the pattern: ')
+pattern_size = int(input('Enter the size of the pattern: '))
 output_count = 0
 while int(pattern_size) > output_count:
     output_count += 1
-    for size in pattern_size:
+    for size in str(pattern_size):
         print(f'*' * int(pattern_size))
