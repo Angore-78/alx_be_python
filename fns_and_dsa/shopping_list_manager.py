6ea407cafc24adf0fb73a0cd0484310c0d8c60c0
Angore-78 <allan.angore@gmail.com>
2025-07-26 11:10:47 +0300

    create shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,36 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True :
+        display_menu()
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            item = input('Which item are you adding? -> ')
+            shopping = shopping_list.append(item) 
+            return shopping_list
+        elif choice == '2':
+            item = input('Which item are you taking out? -> ')
+            if item not in shopping_list:
+                print(f"Sorry,but{item} is not in your shopping list")
+                return shopping_list
+            else: 
+                shopping =shopping_list.remove(item)
+                return shopping_list
+        if choice == '3':
+            shopping_list
+            return shopping_list
+        elif choice == '4':
+            print("Goodbye!")
+            break
+              
+               
+
+
+cart = main()
+print(cart)
