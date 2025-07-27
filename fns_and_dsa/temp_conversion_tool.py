d1939bc939fa0c190869fc44ac648c4726c7aa06
Angore-78 <allan.angore@gmail.com>
2025-07-27 12:07:21 +0300

    create temp_conversion_tool.py and explore_datetime.py

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,40 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+
+def convert_to_celsius(temperature):
+         converted_temperature = temperature * FAHRENHEIT_TO_CELSIUS_FACTOR
+         return converted_temperature
+
+
+def convert_to_fahrenheit(temperature):
+         converted_temperature = temperature * CELSIUS_TO_FAHRENHEIT_FACTOR
+         return converted_temperature
+
+
+def main():
+     try:
+          temperature_input = input('Enter the temperature to convert: ')
+          temperature = float(temperature_input)
+     except ValueError:
+            print('Kindly enter a numerical value.')
+            return
+     
+     units = input('Is this temperature in Celsius or Fahrenheit? (C/F): ').upper()
+     if units=='C':
+            converted_temperature = convert_to_fahrenheit(temperature)
+            print(f'{temperature} is {converted_temperature}')
+     elif units=='F':
+            converted_temperature=convert_to_celsius(temperature)
+            print(f'{temperature} is {converted_temperature}')
+     else:
+            print('Invalid value for unit.')
+
+
+if __name__ == "__main__":
+       main()
+
+
+        
+
+    
