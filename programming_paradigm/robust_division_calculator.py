d14260eb9121d8bf2839bdb08caea07e41e3f1e8
Angore-78 <allan.angore@gmail.com>
2025-08-01 17:12:34 +0300

    create robust_division_calculator.py

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,30 @@
+def safe_divide(numerator, denominator):
+    try :
+        quotient = float(numerator) / float(denominator)
+        return quotient
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+    except ValueError:
+        print('Error: Please enter numeric values only.')
+    
+
+
+import sys
+from robust_division_calculator import safe_divide
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <numerator> <denominator>")
+        sys.exit(1)
+
+    numerator = sys.argv[1]
+    denominator = sys.argv[2]
+
+    result = safe_divide(numerator, denominator)
+    print(result)
+
+if __name__ == "__main__":
+    main()
+
+    
+    
