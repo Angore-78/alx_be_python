d9730238758dcc92baf4daf031664b1184551616
Angore-78 <allan.angore@gmail.com>
2025-08-03 11:36:16 +0300

    correct library_managent.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -9,7 +9,7 @@ class Library(Book):
     def __init__(self,title,author):
         super.__init__(author,title)
     def __init__(self):
-        self._book=[]
+        self._book= []
     def add_book(self,title):
         self._book.append(Book)
         return self._book
@@ -17,10 +17,11 @@ class Library(Book):
     def check_out_book(self,title):
         self._book.remove(Book)
         return self._book
-    def return_book(self):
-        self._book.append(Book.title())
+    def return_book(self,):
+        self._book.append(Book)
         return self._book
     def list_available_books(self):
+        self._book= []
         return self._book
 
     
