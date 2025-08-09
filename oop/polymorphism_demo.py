29b2ab40c46ee803e0779ad9f190065a9da0f7ec
Angore-78 <allan.angore@gmail.com>
2025-08-09 13:21:10 +0300

    correct polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -8,9 +8,9 @@ class Circle(Shape):
             super().__init__()
             self.radius=radius
         def area(self):
-            return math.pi*self.radius**2
+            return math.pi*self.radius** 2
         
-        
+
 class Rectangle(Shape):
     def __init__(self,length,width):
         super().__init__()
